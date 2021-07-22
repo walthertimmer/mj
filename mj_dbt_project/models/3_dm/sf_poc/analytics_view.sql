7a5a06413e8b0ffa9e48537ab38839ff1716e9af
W.D. Timmer <31596984+walthertimmer@users.noreply.github.com>
2021-07-22 17:10:16 +0200

    Create analytics_view.sql

---
+++ mj_dbt_project/models/3_dm/sf_poc/analytics_view.sql
@@ -0,0 +1,21 @@
+SELECT
+  spa.supplier_name,
+  spa.account_balance as supplier_account_balance,
+  cnra.account_balance as customer_account_balance,
+  cnra.avg_acct_bal_by_nation as average_account_balance_by_nation,
+  spa.available_quantity,
+  spa.average_available_quantity,
+  spa.supply_cost,
+  spa.average_supply_cost,
+  spa.part_name,
+  spa.part_manufacturer,
+  spa.part_brand,
+  spa.part_type,
+  spa.part_size,
+  cnra.nation,
+  cnra.region,
+  cnra.market_segment,
+  cnra.customer_name
+FROM {{ref('supplier_parts_aggregates')}} AS spa
+  JOIN {{ref('customer_nation_region_aggregates')}} AS cnra
+    ON spa.nation_key = cnra.nation_key
